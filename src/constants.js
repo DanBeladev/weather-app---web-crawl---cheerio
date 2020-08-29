8cb77aeaee866f67ef4d9ab03392f3aab6af0901
Dan <danbeladev@gmail.com>
2020-08-30 02:12:09 +0300

    update server url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
-export const API_URL = 'http://localhost:3001/moveo-webcrawl/';
+// export const API_URL = 'http://localhost:3001/moveo-webcrawl/';
+export const API_URL = 'https://moveo-weather-backend.herokuapp.com/moveo-webcrawl/';
+
 // Locations
 export const TEL_AVIV = 'Tel-Aviv Israel';
 export const MADRID = 'Madrid Spain';
