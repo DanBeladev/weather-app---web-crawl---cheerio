e8695114e1c896308e263af23c067739e954235b
Dan <danbeladev@gmail.com>
2020-08-30 02:39:02 +0300

    arrange package.json

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,4 @@
-// export const API_URL = 'http://localhost:3001/moveo-webcrawl/';
 export const API_URL = 'https://moveo-weather-backend.herokuapp.com/moveo-webcrawl/';
-
 // Locations
 export const TEL_AVIV = 'Tel-Aviv Israel';
 export const MADRID = 'Madrid Spain';
